1c6eef997db22281588f36cafcc7903054d72430
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-06-15 21:43:42 -0300

    First?

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aluracursos.screenmatch.repository;
-
-import com.aluracursos.screenmatch.model.Serie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SerieRepository extends JpaRepository<Serie,Long> {
-}
